229e44a9201e5989a91e02cc3b4b4bd742f5bfcf
kurt6783 <k0911245920@gmail.com>
2020-02-19 10:46:42 +0800

    取消Item/Board的view

--- application/views/header/header.php
+++ application/views/header/header.php
@@ -5,38 +5,14 @@
 	<link rel="icon" type="image/x-icon" href="https://s2.coinmarketcap.com/static/img/coins/200x200/2099.png?_=cb31027">		
 	<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body style="padding-top:50px">	
-	<!-- <div class="header sticky-top reduce-header">
-		<nav class="navbar navbar-expand-md navbar-light text-dark align-items-center justify-content-md-between justify-content-around">
-			<div class="container d-flex align-items-center">
-				<a class="ssite-logo order-md-1 order-2 mr-auto d-none d-md-block" href="http://127.0.0.1/CI/home">Home</a>
-				<div class="navbar-collapse order-md-2 order-4 nav-md-style position-relative collapse">
-					<ul class="navbar-nav ml-auto">
-						<li class="nav-item">
-							<a class="reduce-padding nav-link link-white py-md-4 px-md-2 font-weight-bold posotion-relative" href="http://127.0.0.1/CI/news">
-								<span class="nav-link-span">
-									"News"
-									::after
-								</span>
-							</a>
-						</li>
-						
-					</ul>
-				</div>
-			</div>
-		</nav>
-	</div>
-	 -->
-
-
-
+<body style="padding-top:50px">		
 	<div>
 		<nav class="navbar-fixed-top"> 
-            <a class="btn" href="http://127.0.0.1/CI/home">Home</a>
-            <a class="btn" href="http://127.0.0.1/CI/news">News</a>
-            <a class="btn" href="http://127.0.0.1/CI/board">Board</a>
-            <a class="btn" href="http://127.0.0.1/CI/item">Item</a>
-            <a class="btn" href="http://127.0.0.1/CI/register">Register</a>        
+            <a class="btn" href="http://127.0.0.1/web/home">Home</a>
+            <!-- <a class="btn" href="http://127.0.0.1/web/news">News</a> -->
+            <!-- <a class="btn" href="http://127.0.0.1/web/board">Board</a> -->
+            <!-- <a class="btn" href="http://127.0.0.1/web/item">Item</a> -->
+            <a class="btn" href="http://127.0.0.1/web/register">Register</a>        
     	</nav>
 	</div>
 	
